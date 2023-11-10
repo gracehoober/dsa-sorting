282c1037e35ca042e94f4ca3df0834f5bb032d5c
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-11-10 11:24:38 -0800

    merge in progress

--- merge.js
+++ merge.js
@@ -1,5 +1,31 @@
-function merge() {}
+function merge(a, b) {
+  //combined array
+   // i
+   // j
+  //while  i and j less than arr.length
+    //if a[0] <= b[0]
+      //put a[i] into new array
+      // i ++
+    // else
+      //put b[j] into new array
+      // j ++
 
-function mergeSort() {}
+  //if(i > arr.length) put the rest of a in
+    //same with b
+  let combined = [];
+  let i = 0
+  let j = 0
+  while( i < arr.length && j < arr.length){
+    if(a[i] <= b[i]){
+      combined.push(a[i]);
+      i++
+    }
+  }
+
+}
+
+function mergeSort() {
+
+}
 
 module.exports = { merge, mergeSort};
